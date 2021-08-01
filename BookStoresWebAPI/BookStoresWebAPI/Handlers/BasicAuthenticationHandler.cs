b319090da7bb1904092b2cc1ae5420fa18279eae
caoquangtrong <caoquangtrong11a1@gmail.com>
2021-08-01 22:58:54 +0700

    Add db and demo basic Authentication

---
+++ BookStoresWebAPI/BookStoresWebAPI/Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,63 @@
+﻿using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly BookStoresDBContext _context;
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            BookStoresDBContext context,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+            _context = context;
+        }
+
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            User user = null;
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                var email = credentials[0];
+                var password = credentials[1];
+
+                user = _context.User.Where(user => user.EmailAddress == email && user.Password == password).FirstOrDefault();
+                if (user == null)
+                    return AuthenticateResult.Fail("Invalid Email or Password");
+                else
+                {
+                    var claims = new Claim[] {new Claim(ClaimTypes.Name, user.EmailAddress)};
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                }
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+            return AuthenticateResult.Fail("");
+        }
+    }
+}
